efc2386cb2732363216d2478b66bdc8bc8520342
Hoaidong32 <dothihoaidong@gmail.com>
2026-07-31 09:25:40 +0700

    style: format VerifyOtpRequest

--- backend/src/main/java/com/haui/lms/dto/VerifyOtpRequest.java
+++ backend/src/main/java/com/haui/lms/dto/VerifyOtpRequest.java
@@ -4,6 +4,7 @@ import jakarta.validation.constraints.*;
 import lombok.Getter;
 import lombok.Setter;
 import com.haui.lms.constant.ErrorMessage;
+
 @Getter
 @Setter
 public class VerifyOtpRequest {
