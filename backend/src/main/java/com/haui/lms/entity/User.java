71cce641bd76cfcbbdb88d09f7e4cfa5149d4a60
Hoaidong32 <dothihoaidong@gmail.com>
2026-07-31 09:23:50 +0700

    fix: add lombok annotations for User entity

--- backend/src/main/java/com/haui/lms/entity/User.java
+++ backend/src/main/java/com/haui/lms/entity/User.java
@@ -14,6 +14,9 @@ import java.util.UUID;
 @Table(name = "users")
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class User extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
