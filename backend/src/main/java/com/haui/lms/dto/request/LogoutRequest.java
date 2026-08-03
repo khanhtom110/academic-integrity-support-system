2c942e656c1834ba367a14096bc922b4bb28d973
kdieu4 <hoangthanhdieu2006@gmail.com>
2026-08-03 16:46:24 +0700

    fix(auth): update Brevo API keys and SMTP environment configurations

--- backend/src/main/java/com/haui/lms/dto/request/LogoutRequest.java
+++ backend/src/main/java/com/haui/lms/dto/request/LogoutRequest.java
@@ -3,5 +3,9 @@ package com.haui.lms.dto.request;
 import com.haui.lms.constant.ErrorMessage;
 import jakarta.validation.constraints.NotBlank;
 
-public record LogoutRequest(@NotBlank(message = ErrorMessage.NOT_BLANK_FIELD) String refreshToken) {
+public record LogoutRequest(
+// @formatter:off
+        @NotBlank(message = ErrorMessage.NOT_BLANK_FIELD)
+        String refreshToken
+) {
 }
