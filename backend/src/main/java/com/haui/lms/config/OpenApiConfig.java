1314a9d867237b8b0b9438c3ab0e3f3ce81ff126
Nguyen Viet Khanh <lemontre3x1@gmail.com>
2026-08-04 17:47:43 +0700

    feat: Using Oauth2 in login Outlook module

--- backend/src/main/java/com/haui/lms/config/OpenApiConfig.java
+++ backend/src/main/java/com/haui/lms/config/OpenApiConfig.java
@@ -1,5 +1,7 @@
 package com.haui.lms.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
@@ -8,6 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@OpenAPIDefinition(servers = { @Server(url = "https://lms-haui.fit", description = "Default Server URL") })
 public class OpenApiConfig {
     @Bean
     public OpenAPI customOpenAPI() {
